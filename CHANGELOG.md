c8431588e660118671ff8ec654041b52fac651a5
Mike Pirog <mike@lando.dev>
2023-06-16 08:38:55 -0400

    evasive manuveur pattern riker alpha 1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,3 @@
-## v2.0.0-beta.3 - [June 13, 2023](https://github.com/lando/setup-lando/releases/tag/v2.0.0-beta.3)
+## v2.0.0-beta.1 - [June 15, 2023](https://github.com/lando/prepare-release-action/releases/tag/v2.0.0-beta.1)
 
-* Added `lando-version` support for `**preview**` branches
-* Added `lando-version` support for URLs
-
-## v2.0.0-beta.2 - [May 5, 2023](https://github.com/lando/setup-lando/releases/tag/v2.0.0-beta.2)
-
-* Added logic around `telemetry`
-* Added `v3` `dependency-check` logix
-
-## v2.0.0-beta.1 - [May 1, 2023](https://github.com/lando/setup-lando/releases/tag/v2.0.0-beta.1)
-
-* Initial release. See [README.md](https://github.com/lando/setup-lando).
+* Executed evasive manuveur pattern Riker beta
---
+++ prepare-release.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const fs = require('fs');
+const getInputs = require('./lib/get-inputs');
+const path = require('path');
+const semverClean = require('semver/functions/clean');
+const semverValid = require('semver/functions/valid');
+
+const {execSync} = require('child_process');
+
+const main = async () => {
+  // start by getting the inputs
+  const inputs = getInputs();
+
+  // dev
+  inputs.version = 'v1.0.0-riker.beta.1';
+  inputs.syncEmail = 'mike@lando.dev';
+  inputs.syncUsername = 'Mike Pirog';
+  inputs.syncBranch = 'pattern-riker-beta';
+
+  try {
+    // validate that we have a version
+    if (!inputs.version) throw new Error('Version is a required input!');
+    // and that it is semantically valid
+    if (semverValid(semverClean(inputs.version)) === null) throw new Error('Version must be semver valid!');
+    // and that we have a package.json
+    if (!fs.existsSync(`${inputs.root}/package.json`)) throw new Error(`Could not detect a package.json in ${inputs.root}`);
+
+    // add local node bin to path so we can make use of stuff weve installed
+    core.addPath(path.resolve(__dirname, 'node_modules', '.bin'));
+
+    // configure git
+    core.startGroup('Configuring git');
+    // get status of shallowness
+    const isShallow = execSync('git rev-parse --is-shallow-repository', {maxBuffer: 1024 * 1024 * 10, encoding: 'utf-8'});
+    // set user/email
+    await exec.exec('git', ['config', 'user.name', inputs.syncUsername]);
+    await exec.exec('git', ['config', 'user.email', inputs.syncEmail]);
+    // if a shallow repo then unshallow and fetch all
+    if (isShallow.trim() === 'true') {
+      await exec.exec('git', ['fetch', '--unshallow']);
+      await exec.exec('git', ['fetch', '--all']);
+    }
+    // check out correct branch
+    await exec.exec('git', ['checkout', inputs.syncBranch]);
+    core.endGroup();
+
+    // - name: Bundle dependencies
+    //   shell: bash
+    //   if: ${{ inputs.bundle-deps == "true" }}
+    //   run: |
+    //     bundle-dependencies update
+    //     echo "Bundled dependencies:"
+    //     bundle-dependencies list-bundled-dependencies
+    // - name: Lando Plugin stuff
+    //   if: ${{ inputs.validate-plugin == "true" }}
+    //   uses: actions/github-script@v6
+    //   env:
+    //     PJSON_LOCATION: ${{ github.workspace }}/package.json
+    //   with:
+    //     script: |
+    //       const pjsonLocation = process.env.PJSON_LOCATION;
+    //       const pjson = require(pjsonLocation);
+    //       const hasLandoKeyword = Array.isArray(pjson.keywords) && (pjson.keywords.includes("lando") || pjson.keywords.includes("lando-plugin"));
+    //       if (hasLandoKeyword) {
+    //         // Add in GitHub output https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions
+    //         core.info("Confirmed this is a Lando plugin.")
+    //         process.exit(0);
+    //       } else {
+    //         core.setFailed("Invalid Lando plugin; required keywords not detected in package.json.");
+    //       }
+
+    // running user specified commands
+    core.startGroup('Running commands');
+    for (const command of inputs.commands) await exec.exec(command);
+    core.endGroup();
+
+    // bumping version
+    await exec.exec('bump', [inputs.version, '--commit', inputs.syncMessage, '--all']);
+    // tagging commits
+    for (const tag of inputs.syncTags.concat([inputs.version])) {
+      await exec.exec('git', ['tag', '--force', tag, '$(git log --pretty=format:"%h" -n 1)']);
+    }
+
+    // log where we are at before we sync
+    core.startGroup('Change information');
+    await exec.exec('git', ['--no-pager', 'log', '-1']);
+    await exec.exec('git', ['--no-pager', 'tag', '--points-at', 'HEAD']);
+    await exec.exec('git', ['diff', 'HEAD~1']);
+    core.endGroup();
+
+    // sync back to repo
+    // # git push origin ${{ github.event.release.target_commitish }}
+    // # git push --force origin ${tag}
+
+  // catch
+  } catch (error) {
+    core.setFailed(error.message);
+    process.exit(1);
+  }
+};
+
+// main logix
+main();
