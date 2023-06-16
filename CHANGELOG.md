30c4e1867edc85de58731c7c7b73b05254815d0f
Mike Pirog <mike@lando.dev>
2023-06-16 12:49:58 -0400

    evasive manuveur pattern riker beta 1 (#1)
    * evasive manuveur pattern riker beta 1
    
    * evasive manuveur pattern riker beta 2
    
    * evasive manuveur pattern riker beta 3
    
    * evasive manuveur pattern riker beta 4
    
    * evasive manuveur pattern riker beta 5
    
    * evasive manuveur pattern riker beta 6
    
    * evasive manuveur pattern riker beta 7
    
    * evasive manuveur pattern riker beta 8
    
    * evasive manuveur pattern riker beta 9
    
    * evasive manuveur pattern riker beta 10
    
    * evasive manuveur pattern riker beta 11
    
    * evasive manuveur pattern riker beta 12
    
    * evasive manuveur pattern riker beta 13
    
    * evasive manuveur pattern riker beta 14
    
    * evasive manuveur pattern riker beta 15
    
    * evasive manuveur pattern riker beta 16
    
    * evasive manuveur pattern riker beta 17
    
    * evasive manuveur pattern riker beta 18
    
    * evasive manuveur pattern riker beta 19
    
    * evasive manuveur pattern riker beta 20
    
    * evasive manuveur pattern riker beta 21
    
    * evasive manuveur pattern riker beta 22
    
    * evasive manuveur pattern riker beta 23
    
    * evasive manuveur pattern riker beta 23
    
    * evasive manuveur pattern riker beta 24
    
    * evasive manuveur pattern riker beta 25
    
    * evasive manuveur pattern riker beta 26
    
    * evasive manuveur pattern riker beta 27
    
    * evasive manuveur pattern riker beta 28
    
    * evasive manuveur pattern riker beta 29
    
    * evasive manuveur pattern riker beta 30
    
    * evasive manuveur pattern riker beta 31
    
    * evasive manuveur pattern riker beta 32
    
    * evasive manuveur pattern riker beta 33
    
    * evasive manuveur pattern riker beta 34
    
    * evasive manuveur pattern riker beta 35
    
    * evasive manuveur pattern riker beta 36
    
    * evasive manuveur pattern riker beta 37
    
    * evasive manuveur pattern riker beta 38
    
    * evasive manuveur pattern riker beta 39
    
    * evasive manuveur pattern riker beta 40
    
    * evasive manuveur pattern riker beta 41
    
    * evasive manuveur pattern riker beta 42
    
    * evasive manuveur pattern riker beta 43
    
    * evasive manuveur pattern riker beta 44
    
    * evasive manuveur pattern riker beta 45
    
    * evasive manuveur pattern riker beta 46
    
    * evasive manuveur pattern riker beta 47
    
    * evasive manuveur pattern riker alpha 1
    
    * evasive manuveur pattern riker alpha 2
    
    * evasive manuveur pattern riker alpha 3
    
    * evasive manuveur pattern riker alpha 4
    
    * evasive manuveur pattern riker alpha 5
    
    * evasive manuveur pattern riker alpha 6
    
    * evasive manuveur pattern riker alpha 7
    
    * evasive manuveur pattern riker alpha 8
    
    * evasive manuveur pattern riker alpha 9
    
    * evasive manuveur pattern riker alpha 10
    
    * evasive manuveur pattern riker alpha 11
    
    * evasive manuveur pattern riker alpha 12
    
    * evasive manuveur pattern riker alpha 13
    
    * evasive manuveur pattern riker alpha 14
    
    * evasive manuveur pattern riker alpha 15
    
    * evasive manuveur pattern riker alpha 16
    
    * evasive manuveur pattern riker alpha 17
    
    * evasive manuveur pattern riker alpha 18
    
    * evasive manuveur pattern riker alpha 19
    
    * evasive manuveur pattern riker alpha 20
    
    * evasive manuveur pattern riker alpha 21
    
    * evasive manuveur pattern riker alpha 22
    
    * evasive manuveur pattern riker alpha 23
    
    * evasive manuveur pattern riker alpha 24
    
    * evasive manuveur pattern riker alpha 25
    
    * evasive manuveur pattern riker alpha 26
    
    * evasive manuveur pattern riker alpha 27
    
    * evasive manuveur pattern riker alpha 28
    
    * evasive manuveur pattern riker alpha 29
    
    * evasive manuveur pattern riker alpha 30
    
    * evasive manuveur pattern riker alpha 31
    
    * evasive manuveur pattern riker alpha 32

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## v2.0.0-beta.1 - [June 15, 2023](https://github.com/lando/prepare-release-action/releases/tag/v2.0.0-beta.1)
+
+* Executed evasive manuveur pattern Riker beta
---
+++ prepare-release.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const fs = require('fs');
+const getInputs = require('./lib/get-inputs');
+const isLandoPlugin = require('./lib/is-lando-plugin');
+const jsonfile = require('jsonfile');
+const path = require('path');
+const semverClean = require('semver/functions/clean');
+const semverValid = require('semver/functions/valid');
+
+const {execSync} = require('child_process');
+
+const main = async () => {
+  // start by getting the inputs
+  const inputs = getInputs();
+  // add more
+  inputs.pjson = path.join(inputs.root, 'package.json');
+
+  try {
+    // validate that we have a version
+    if (!inputs.version) throw new Error('Version is a required input!');
+    // and that it is semantically valid
+    if (semverValid(semverClean(inputs.version)) === null) throw new Error('Version must be semver valid!');
+    // and that we have a package.json
+    if (!fs.existsSync(inputs.pjson)) throw new Error(`Could not detect a package.json in ${inputs.root}`);
+
+    // add local node bin to path so we can make use of stuff weve installed
+    // @NOTE: this location differs based on how we are calling it eg dist/index.js or directly
+    if (fs.existsSync(path.resolve(__dirname, '..', 'node_modules', '.bin'))) {
+      core.addPath(path.resolve(__dirname, '..', 'node_modules', '.bin'));
+    } else {
+      core.addPath(path.resolve(__dirname, 'node_modules', '.bin'));
+    }
+
+    // configure git
+    core.startGroup('Configuring git');
+    // get status of shallowness
+    const isShallow = execSync('git rev-parse --is-shallow-repository', {maxBuffer: 1024 * 1024 * 10, encoding: 'utf-8'});
+    // set user/email
+    await exec.exec('git', ['config', 'user.name', inputs.syncUsername]);
+    await exec.exec('git', ['config', 'user.email', inputs.syncEmail]);
+    // if a shallow repo then unshallow and fetch all
+    if (isShallow.trim() === 'true') {
+      await exec.exec('git', ['fetch', '--unshallow']);
+      await exec.exec('git', ['fetch', '--all']);
+    }
+    // check out correct branch
+    await exec.exec('git', ['checkout', inputs.syncBranch]);
+    core.endGroup();
+
+    // if using landoPlugin ez-mode then also bundle deps
+    if (inputs.landoPlugin) {
+      await exec.exec('bundle-dependencies', ['update']);
+      await exec.exec('bundle-dependencies', ['list-bundled-dependencies']);
+    }
+
+    // run user specified commands
+    for (const command of inputs.commands) await exec.exec(command);
+
+    // bump version
+    await exec.exec('bump', [inputs.version, '--commit', inputs.syncMessage, '--all']);
+
+    // get helpful stuff
+    const currentCommit = execSync('git log --pretty=format:\'%h\' -n 1', {maxBuffer: 1024 * 1024 * 10, encoding: 'utf-8'});
+    const tags = inputs.syncTags.concat([inputs.version]);
+
+    // tag commits
+    for (const tag of tags) await exec.exec('git', ['tag', '--force', tag, currentCommit.trim()]);
+
+    // if using landoPlugin ez-mode then validate lando plugin
+    if (inputs.landoPlugin && !isLandoPlugin(jsonfile.readFileSync(inputs.pjson))) {
+      throw new Error('Does not appear to be a valid Lando plugin! package.json must contain a lando key or the lando-plugin keyword');
+    }
+
+    // log where we are at before we sync
+    core.startGroup('Change information');
+    await exec.exec('git', ['--no-pager', 'log', '-1']);
+    await exec.exec('git', ['--no-pager', 'tag', '--points-at', 'HEAD']);
+    await exec.exec('git', ['diff', 'HEAD~1']);
+    core.endGroup();
+
+    // sync back to repo
+    if (inputs.sync) {
+      await exec.exec('git', ['push', 'origin', inputs.syncBranch]);
+      for (const tag of tags) await exec.exec('git', ['push', '--force', 'origin', tag]);
+    }
+
+  // catch
+  } catch (error) {
+    core.setFailed(error.message);
+    process.exit(1);
+  }
+};
+
+// main logix
+main();
