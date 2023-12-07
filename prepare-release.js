30cf735e4bc8fa99a195abadcd7399788960560f
Mike Pirog <mike@lando.dev>
2023-12-07 10:44:06 -0500

    lando-plugin sets dist correctly, and also allows meta to be added

--- prepare-release.js
+++ prepare-release.js
@@ -10,6 +10,7 @@ const jsonfile = require('jsonfile');
 const path = require('path');
 const semverClean = require('semver/functions/clean');
 const semverValid = require('semver/functions/valid');
+const set = require('lodash.set');
 
 const main = async () => {
   // start by getting the inputs
@@ -56,14 +57,50 @@ const main = async () => {
     // set user/email
     await exec.exec('git', ['config', 'user.name', inputs.syncUsername]);
     await exec.exec('git', ['config', 'user.email', inputs.syncEmail]);
-    // check out correct branch
-    await exec.exec('git', ['checkout', inputs.syncBranch]);
+
+    // check out correct branch if we plan to sync back later
+    if (inputs.sync) await exec.exec('git', ['checkout', inputs.syncBranch]);
     core.endGroup();
 
     // run user specified commands
     for (const command of inputs.commands) await exec.exec(command);
 
-    // bump version
+    // if "lando-plugin" is on then lets add some required meta if we need to
+    // note that we put this stuff at the start of meta so that a users meta will take priority
+    // in the case that they set the same stuff
+    if (inputs.landoPlugin && !isLandoPlugin(jsonfile.readFileSync(inputs.pjson))) {
+      const pjson = jsonfile.readFileSync('package.json');
+      const keywords = pjson.keywords ?? [];
+      inputs.meta.unshift(`keywords[${keywords.length}]=lando-plugin`);
+    }
+
+    // apply any metadata to the package.json
+    if (inputs.meta.length > 0) {
+      const pjson = jsonfile.readFileSync(inputs.pjson);
+
+      // go through meta and update package.json
+      inputs.meta.forEach(line => {
+        if (Array.isArray(line)) set(pjson, line[0], line[1]);
+        else if (typeof line === 'string') {
+          const key = line.split('=')[0];
+          const value = line.split('=')[1];
+          set(pjson, key, value);
+        }
+      });
+
+      // write and debug
+      jsonfile.writeFileSync(inputs.pjson, pjson);
+      core.debug(`updated pjson`);
+      core.debug(jsonfile.readFileSync(inputs.pjson));
+    }
+
+    // if using landoPlugin ez-mode then validate lando plugin
+    // it should be impossible to get here at this point but lets keep this just in case
+    if (inputs.landoPlugin && !isLandoPlugin(jsonfile.readFileSync(inputs.pjson))) {
+      throw new Error('Does not appear to be a valid Lando plugin! package.json must contain a lando key or the lando-plugin keyword');
+    }
+
+    // bump version AND commit everything changed
     await exec.exec(`${binDir}/bump`, [inputs.version, '--commit', inputs.syncMessage, '--all']);
 
     // get helpful stuff, for some reasons windows interprets the format wrapping quptes literally?
@@ -73,28 +110,53 @@ const main = async () => {
     // tag commits
     for (const tag of tags) await exec.exec('git', ['tag', '--force', tag, currentCommit]);
 
-    // if using landoPlugin ez-mode then validate lando plugin
-    if (inputs.landoPlugin && !isLandoPlugin(jsonfile.readFileSync(inputs.pjson))) {
-      throw new Error('Does not appear to be a valid Lando plugin! package.json must contain a lando key or the lando-plugin keyword');
-    }
-
-    // log where we are at before we sync
-    core.startGroup('Change information');
-    await exec.exec('git', ['--no-pager', 'log', '-1']);
-    await exec.exec('git', ['--no-pager', 'tag', '--points-at', 'HEAD']);
-    await exec.exec('git', ['diff', 'HEAD~1']);
-    core.endGroup();
-
     // sync back to repo if applicable
     if (inputs.sync) {
+      // log where we are at before we sync
+      core.startGroup('Change information');
+      await exec.exec('git', ['--no-pager', 'log', '-1']);
+      await exec.exec('git', ['--no-pager', 'tag', '--points-at', 'HEAD']);
+      await exec.exec('git', ['diff', 'HEAD~1']);
+      core.endGroup();
+
+      // N*SYNC
       await exec.exec('git', ['push', 'origin', inputs.syncBranch]);
       for (const tag of tags) await exec.exec('git', ['push', '--force', 'origin', tag]);
     }
 
-    // finally bundle deps if we need to
+    // bundle deps if we need to
+    //
+    // this happens AFTER sync because we ASSUME you do not have your node_modules checks into your repo
+    // and if you do then this should be in your package.json already
     if (inputs.bundleDependencies) {
       await exec.exec(`${binDir}/bundle-dependencies`, ['update']);
       await exec.exec(`${binDir}/bundle-dependencies`, ['list-bundled-dependencies']);
+    }
+
+    // lets also add the "dist" information, this key is required if you want the plugin to be updateable
+    // via lando update
+    //
+    // this also happens after sync and  SHOULD NOT EVER be added manually to your package.json
+    if (inputs.landoPlugin) {
+      const pjson = jsonfile.readFileSync(inputs.pjson);
+
+      // onyl add information if dist isnt already in there
+      if (!pjson.dist) {
+        try {
+          const dist = JSON.parse(getStdOut(`npm pack --json --dry-run`));
+          const {integrity, shasum, fileCount, unpackedSize, filename} = dist[0];
+          pjson.dist = {integrity, shasum, filename, fileCount, unpackedSize};
+        } catch (error) {
+          core.warning(`error getting dist information, not fatal, setting dist to ${inputs.version} instead`);
+          core.debug(error);
+          pjson.dist = inputs.version;
+        }
+
+        // write and debug
+        jsonfile.writeFileSync(inputs.pjson, pjson);
+        core.debug(`added dist info to pjson`);
+        core.debug(jsonfile.readFileSync(inputs.pjson));
+      }
     }
 
   // catch
